211bba243db551c7120b4b3c69298f06b5cb6510
Maxldc <1060094792@qq.com>
2024-11-14 22:03:36 +0800

    5.1/Q5.1.1.1. 几何画板/ans.txt 5.1/Q5.1.1.1. 几何画板/main.cpp 5.1/Q5.1.1.1. 几何画板/main.exe 5.1/Q5.1.1.1. 几何画板/map.py

--- 5.1/Q5.1.1.1. 几何画板/main.cpp
+++ 5.1/Q5.1.1.1. 几何画板/main.cpp
@@ -4,13 +4,15 @@
 #include <complex>
 #include <iostream>
 using namespace std;
-const int N = 5e4 + 9;
 typedef complex<double> comp;
-int T, cnt;
-comp c[N];
 struct Edge {
     comp u, v;
-} e[N];
+} ;
+const int N = 5e4 + 9;
+int T, cnt;
+double ang[N];
+comp c[N];
+Edge e[N];
 string s;
 double S(comp x, comp y, comp z) {
     comp a = y - x, b = z - x;
@@ -23,7 +25,7 @@ double dist(comp x, comp y) {
 int main() {
     // ios::sync_with_stdio(false);
     // cin.tie(0);
-    freopen("3.in", "r", stdin);
+    freopen("4.in", "r", stdin);
     freopen("ans.txt", "w", stdout);
     scanf("%d", &T);
     for (int i = 1; i <= T; i++) {
@@ -59,6 +61,25 @@ int main() {
             scanf("%d%d", &x, &y);
             comp a = (e[y].v - e[y].u) * (comp){ 0, 1 } + c[x];
             e[i] = { c[x], a };
+        }
+        if (s == "angle") {
+            int x, y;
+            scanf("%d%d", &x, &y);
+            ang[i] = abs(arg(c[y]) - arg(c[x]));
+            printf("point1:%lf %lf\n", c[x].real(), c[x].imag());
+            printf("point2:%lf %lf\n", c[y].real(), c[y].imag());
+        }
+        if (s == "rotate") {
+            int x, y, z;
+            scanf("%d%d%d", &x, &y, &z);
+            printf("point1:%lf %lf\n", c[x].real(), c[x].imag());
+            printf("point2:%lf %lf\n", c[y].real(), c[y].imag());
+            printf("angle:%lf\n", ang[z]);
+            comp an = {cos(ang[i]), sin(ang[i])};
+            comp a = c[x] - c[y];
+            a *= an;
+            c[i] = a + c[y];
+            printf("%.10lf %.10lf\n", c[i].real(), c[i].imag());
         }
     }
     return 0;
